60eb0d00f2d7bfaea3927b40a19def31963cf6a7
Hooriya M. Fareed <parih3080@gmail.com>
2024-11-11 17:56:46 +0500

    push_changes

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -5,13 +5,13 @@ import image2 from "../../public/assests/projects/calculator.png";
 import image3 from "../../public/assests/projects/amazon_clone.png";
 import image4 from "../../public/assests/projects/e-commerce_website.png";
 import image5 from "../../public/assests/projects/blog-site.png";
-import image6 from "../../public/assests/projects/to-do-app.png";
+import image6 from "../../public/assests/projects/resume_builder.jpeg";
 import Link from "next/link";
 import React from "react";
 
 const Project = () => {
   return (
-    <div id="Projects">
+    <div id="Project">
       <section className="body-font">
         <div
           className="container"
@@ -38,7 +38,7 @@ const Project = () => {
                     Facebook Login Page Clone Project
                   </h2>
                   <h1 className="project-title">Facebook login page clone</h1>
-                  <p className="project-description">
+                  <p className="project-description line-clamp-3">
                     This project was all about honing my front-end skills and
                     focusing on the design aspects of web development:
                   </p>
@@ -65,7 +65,7 @@ const Project = () => {
                     Calculator Web App Project
                   </h2>
                   <h1 className="project-title">Calculator</h1>
-                  <p className="project-description">
+                  <p className="project-description line-clamp-3">
                     This project pushed my web dev skills and was a blast to
                     build! If you love coding or need a cool project idea,
                     building a calculator is a fantastic way to hone your skills
@@ -92,7 +92,7 @@ const Project = () => {
                 <div className="project-overlay">
                   <h2 className="project-category">Amazon Clone Project</h2>
                   <h1 className="project-title">Amazon Clone</h1>
-                  <p className="project-description">
+                  <p className="project-description line-clamp-3">
                     I recreated the Amazon homepage using only HTML and CSS,
                     highlighting sleek design and responsive layouts. It is a
                     powerful example of what can be achieved with pure front-end
@@ -121,7 +121,7 @@ const Project = () => {
                     E-commerce Website Project
                   </h2>
                   <h1 className="project-title">E-commerce Website</h1>
-                  <p className="project-description">
+                  <p className="project-description line-clamp-3">
                     I developed an e-commerce platform that integrates product
                     data for seamless display and management. This includes
                     &apos;Add to Cart&apos; functionality and detailed product
@@ -150,7 +150,7 @@ const Project = () => {
                   <h1 className="project-title">
                     Dynamic Multi-Page Blog site
                   </h1>
-                  <p className="project-description">
+                  <p className="project-description line-clamp-3">
                     I developed a Multi-Page Blog using Next.js and Tailwind
                     CSS, featuring dynamic routing for seamless navigation and
                     an interactive comments section to boost user engagement.
@@ -172,17 +172,23 @@ const Project = () => {
               <div className="project-container">
                 <Image alt="To-do App" className="project-image" src={image6} />
                 <div className="project-overlay">
-                  <h2 className="project-category">To-do App Project</h2>
-                  <h1 className="project-title">To-do App</h1>
-                  <p className="project-description">
-                    I built a To-Do App featuring add, edit, and delete
-                    functionality for tasks. With optimized performance and a
-                    clean, responsive design, this project showcases my ability
-                    to create efficient, user-friendly applications.
+                  <h2 className="project-category">
+                    Shareable-resume-builder Project
+                  </h2>
+                  <h1 className="project-title">Shareable-resume-builder</h1>
+                  <p className="project-description line-clamp-3">
+                    Created a Resume Builder using HTML, CSS, and JavaScript,
+                    featuring customizable templates that allow users to add,
+                    edit, and arrange sections for a professional resume. With a
+                    responsive design and PDF export functionality, this project
+                    showcases my skills in crafting intuitive, front-end
+                    applications.
                   </p>
                   <Link
                     target="_blank"
-                    href="https://to-do-app-by-hooriya.netlify.app/"
+                    href={
+                      "https://shareable-resume-builder-by-hooriya.netlify.app"
+                    }
                   >
                     <p className="project-link">View Project..</p>
                   </Link>
